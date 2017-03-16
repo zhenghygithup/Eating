b9d02c073ff3594fb10eff4eb32f6a7ba0cab751
Olsson <zhenghy53@163.com>
2017-03-16 20:06:30 +0800

    test

---
+++ app/src/main/java/com/eating/administrator/eating/MainActivity.java
@@ -0,0 +1,34 @@
+package com.eating.administrator.eating;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    public void ViewClick(View v) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
